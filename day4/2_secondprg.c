ce9160bbd719b2a69a503cf8f0830f5486a1db80
kumuda_1516 <kumudapriya229@gmail.com>
2025-11-20 09:48:09 +0530

    day4 code pushed

---
+++ day4/2_secondprg.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("hii kumuda");
+    return 0;
+
+}
---
+++ day4/3_variables.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+
+int age =30;
+printf("your age is: %d",age);
+
+float marks = 560.5;
+printf("\nyour marks is: %f",marks);
+
+double percentage =89.5;
+printf("\nyour percentage is:%lf",percentage);
+
+char name = 'p';
+printf("\nyour name is:%c",name);
+
+bool istalking = false;
+printf("\n is\" talk\ting\":%d",istalking);
+
+}
